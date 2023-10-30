b3cd8eb1830efbe60193fc0fb5fe07b16bf7e26d
Gbvy Bvgj <mnbg20033@gmail.com>
2023-10-31 00:22:11 +0500

    все

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -29,12 +29,69 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnStart = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tmr = new System.Windows.Forms.Timer(this.components);
+            this.pct = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pct)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(93, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Exit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // tmr
+            // 
+            this.tmr.Tick += new System.EventHandler(this.tmr_Tick);
+            // 
+            // pct
+            // 
+            this.pct.Image = ((System.Drawing.Image)(resources.GetObject("pct.Image")));
+            this.pct.Location = new System.Drawing.Point(0, 307);
+            this.pct.Name = "pct";
+            this.pct.Size = new System.Drawing.Size(168, 143);
+            this.pct.TabIndex = 2;
+            this.pct.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pct);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pct)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer tmr;
+        private System.Windows.Forms.PictureBox pct;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -16,5 +16,48 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private bool move = false;//переменная определяющая движение
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (move)//если картинка двигается то
+            {//при нажатии она остановится
+                tmr.Stop();
+                btnStart.Text = "Start";
+                move = false;
+            }
+            else if (pct.Left + pct.Width > this.Width)
+            {//если картинка на краю формы то пр нажатии она возвращается в начальное положение
+                pct.Left = 0;
+                move = false;
+            }
+            else
+            {//картинка начинает двигаться
+                tmr.Start();
+                btnStart.Text = "Stop";
+                move = true;
+                tmr.Interval = 50;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void tmr_Tick(object sender, EventArgs e)
+        {
+            pct.Left = pct.Left + 20;//движение картинки
+            if (pct.Left + pct.Width > this.Width)//если картинка коснулась края движение останавливается
+            {
+                tmr.Stop();
+                btnStart.Text = "Start";
+                return;
+            }
+        }
     }
 }
