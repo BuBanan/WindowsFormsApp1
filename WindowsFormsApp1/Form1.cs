d3e24e2066d6ad89333a10af553d36669293dd2d
Gbvy Bvgj <mnbg20033@gmail.com>
2023-11-09 09:25:33 +0500

    ss

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -19,6 +19,8 @@ namespace WindowsFormsApp1
 
         private void btnCalc_Click(object sender, EventArgs e)
         {
+            DateTime currentTime = DateTime.Now; //добавление времени которое сейчас
+            textBox1.Text = currentTime.ToString();
             int fromX = int.Parse(txtX1.Text); //Добавим переменные, которые проассоциируем с данными текстовыми полями
             int toX = int.Parse(txtX2.Text);
             int fromY = int.Parse(txtY1.Text);
@@ -30,18 +32,47 @@ namespace WindowsFormsApp1
             txtX2.Text = "";
         }
         if (fromY > toY)//проверить вводимый диапазон
+        int tmp;//добавляем переменные для минут и секунд
+        int min;
+        private void tmrSecundomer_Tick(object sender, EventArgs e)
         {
             MessageBox.Show("Интервал должен быть от меньшего к большему");
             txtY1.Text = "";
             txtY2.Text = "";
         }
             for (int x = fromX; x <= toX; x++)//5.	Сейчас можно создать вложенный цикл для расчета функции z(x,y) = x - y
+            int tmp = Int32.Parse(txtSeconds.Text);//преобразуем строковое начение в численное
+            int min = Int32.Parse(txtMinues.Text);
+            if (tmp >= 60)//перевод секунд в минуты
             {
                     for (int y = fromY; y <= toY; y++)//6.	Результаты вычислений будут добавляться в — list box
                     { 
                         lstResult.Items.Add($"z(x,y) = {x} - {y} = {x - y}");
                     }
             }
+                min += 1;
+                txtMinues.Text = min.ToString();
+                tmp = 0;
+                txtSeconds.Text = tmp.ToString();
+            }
+            else//увеличение таймера секунд каждый тик
+            {
+                tmp += 1;
+                txtSeconds.Text = tmp.ToString();
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)//запуск и остановка таймера на кнопку
+        {
+            tmrSecundomer.Enabled = !tmrSecundomer.Enabled;
+        }
+
+        private void zero_Click(object sender, EventArgs e)//обнуление показателей таймера
+        {
+            tmp = 0;
+            txtSeconds.Text = tmp.ToString();
+            min = 0;
+            txtMinues.Text = min.ToString();
         }
     }
 }
