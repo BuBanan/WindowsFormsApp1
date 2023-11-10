5fc4eae097991e7b0db9c73cc769b889f38c64cf
Gbvy Bvgj <mnbg20033@gmail.com>
2023-11-11 00:44:14 +0500

    усе

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -28,13 +28,128 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lstFromfile = new System.Windows.Forms.ListBox();
+            this.lst = new System.Windows.Forms.ListBox();
+            this.txt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstFromfile
+            // 
+            this.lstFromfile.FormattingEnabled = true;
+            this.lstFromfile.Location = new System.Drawing.Point(438, 103);
+            this.lstFromfile.Name = "lstFromfile";
+            this.lstFromfile.Size = new System.Drawing.Size(350, 251);
+            this.lstFromfile.TabIndex = 0;
+            // 
+            // lst
+            // 
+            this.lst.FormattingEnabled = true;
+            this.lst.Location = new System.Drawing.Point(12, 103);
+            this.lst.Name = "lst";
+            this.lst.Size = new System.Drawing.Size(374, 251);
+            this.lst.TabIndex = 1;
+            // 
+            // txt
+            // 
+            this.txt.Location = new System.Drawing.Point(12, 33);
+            this.txt.Name = "txt";
+            this.txt.Size = new System.Drawing.Size(100, 20);
+            this.txt.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Путь к файлу";
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(12, 389);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(374, 20);
+            this.txt2.TabIndex = 4;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(12, 415);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(374, 23);
+            this.btnAdd.TabIndex = 5;
+            this.btnAdd.Text = "Добавить элемент в список";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(713, 415);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "Выход";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(438, 360);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(350, 23);
+            this.btnOpen.TabIndex = 7;
+            this.btnOpen.Text = "Загрузить из файла";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(12, 360);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(374, 23);
+            this.btnSave.TabIndex = 8;
+            this.btnSave.Text = "Сохранить в файле";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt);
+            this.Controls.Add(this.lst);
+            this.Controls.Add(this.lstFromfile);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox lstFromfile;
+        private System.Windows.Forms.ListBox lst;
+        private System.Windows.Forms.TextBox txt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnSave;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
 
 namespace WindowsFormsApp1
@@ -15,6 +16,61 @@ namespace WindowsFormsApp1
         public Form1()
         {
             InitializeComponent();
+        }
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            string fileName = txt.Text;// получаем путь к файлу из текстового поля
+            lstFromfile.Items.Clear(); // очищаем ListBox перед открытием нового файла
+            /* используем using для создания FileStream и BinaryReader
+            FileMode.Open позволяет окрыть файл для чтения
+            FileStream позволяет одновременно читать и записывать*/
+            using (FileStream fs = new FileStream(fileName, FileMode.Open))
+            /* создаем BinaryReader для чтения данных из FileStream
+           считывает данные произвольного типа как бинарные*/
+            using (BinaryReader br = new BinaryReader(fs))
+            {
+                /* цикл который будет выполняться до тех пор пока не достигнут конец файла.
+                метод PeekChar() возвращает следующий символ остявляя указатель в той же позиции. */
+                while (br.PeekChar() != -1)
+                {
+                    lstFromfile.Items.Add(br.ReadString());// добавляем в элемент управления ListBox каждую строку прочитанную с использованием BinaryReader.
+                }
+                // закрываем BinaryReader и FileStream
+                br.Close();
+                fs.Close();
+            }
+        }
+        //кнопка добавить
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            // добавляем текст из текстового поля в ListBox и очищаем текстовое поле
+            lst.Items.Add(txt2.Text);
+            txt2.Clear();
+        }
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string fileName = txt.Text; // пуmь к файлу из текстового поля
+            if (File.Exists(fileName))// если файл существуеm
+            {
+                File.Delete(fileName);//удаляем его
+            }
+            // используем using для автоматического закрытия FileStream и BinaryWriter после использования
+            // создаем файл или перезаписываем существующий
+            using (FileStream fs = File.Create(fileName, 1024))
+            using (BinaryWriter bw = new BinaryWriter(fs))// класс для работы с данными файла в двоичной виде
+            {
+                for (var i = 0; i < lst.Items.Count; i++)// пока не конец списка
+                {
+                    bw.Write(lst.Items[i].ToString());// записываем каждый элемент из элемента управления ListBox в файл
+                }
+                // закрываем BinaryWriter и FileStream
+                bw.Close();
+                fs.Close();
+            }
+        }
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
